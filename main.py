f6a1b58749454a2e976a34dfe3fce3115c886e95
Lev <rogulev0805@gmail.com>
2025-04-08 22:50:02 +0300

    add: nice output

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from itertools import combinations
-import matplotlib.pyplot as plt
+
+from scipy.constants import point
 
 
-class Points:
+class PointsXYZ:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
@@ -13,18 +14,29 @@ class Points:
         return f"({self.x}, {self.y}, {self.z})"
 
     def __sub__(self, other):
-        if isinstance(other, Points):
-            return Points(self.x - other.x, self.y - other.y, self.z - other.z)
+        if isinstance(other, PointsXYZ):
+            return PointsXYZ(self.x - other.x, self.y - other.y, self.z - other.z)
         return NotImplemented
 
     def __mul__(self, other):
-        if isinstance(other, Points):
-            return Points(
+        if isinstance(other, PointsXYZ):
+            return PointsXYZ(
                 self.y * other.z - self.z * other.y,
-                - self.x * other.z + self.z * other.x,
+                -self.x * other.z + self.z * other.x,
                 self.x * other.y - self.y * other.x
             )
         return NotImplemented
+
+
+class PointsABCD:
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def __repr__(self):
+        return f"({self.a}, {self.b}, {self.c}, {self.d})"
 
 
 def is_file(filepath):
@@ -36,21 +48,26 @@ def is_file(filepath):
 def read_coordinates(filepath):
     try:
         with open(filepath, 'r') as file:
-            file = file.read()
-        file_format = file[0]
-        coordinates = file[4:].split('\n')
-        points = [Points(*list(map(int, line.strip().split()))) for line in coordinates]
-
+            file_content = file.read()
+        file_format = file_content[0]
+        coordinates = file_content[4:].split('\n')
+        points = []
+        if file_format == 'V':
+            points = [PointsXYZ(*list(map(int, line.strip().split()))) for line in coordinates if line.strip()]
+        elif file_format == 'H':
+            points = [PointsABCD(*list(map(int, line.strip().split()))) for line in coordinates if line.strip()]
         return file_format, points
-    except:
-        pass
+
+    except Exception as e:
+        print(f"Ошибка при чтении файла: {e}")
+        quit(1)
 
 
 def is_one_line(coord):
     a, b, c = coord
-    if c - a == b - a:
-        return True
-    return False
+    vect_ab = b - a
+    vect_ac = c - a
+    return vect_ab * vect_ac == PointsXYZ(0, 0, 0)
 
 
 def count_plane_three_points(coord):
@@ -58,49 +75,100 @@ def count_plane_three_points(coord):
     vect_ab = b - a
     vect_ac = c - a
     vect_normally = vect_ab * vect_ac
-    return lambda x, y, z: (vect_normally.x * (x - c.x) +
-                            vect_normally.y * (y - c.y) +
-                            vect_normally.z * (z - c.z)
-                            ), vect_normally
-    # D = -(N.x * a.x + N.y * a.y + N.z * a.z)
+    D = -(vect_normally.x * a.x + vect_normally.y * a.y + vect_normally.z * a.z)
+    return lambda x, y, z: (vect_normally.x * x + vect_normally.y * y + vect_normally.z * z + D), vect_normally, D
+
 
 def is_one_side(plane, points):
-    if all(plane(point.x, point.y, point.z) for point in points)>=0:
-        return True
-    elif all(plane(point.x, point.y, point.z) for point in points)<=0:
-        return True
-    return False
+    results = [plane(point.x, point.y, point.z) for point in points]
+    if all(r >= 0 for r in results): return 1
+    if all(r <= 0 for r in results): return -1
+    return 0
+
 
 def find_convex_hull(points):
     faces = []
     for three_points in combinations(points, 3):
         if not is_one_line(three_points):
-            plane = count_plane_three_points(three_points)
-            if is_one_side(plane[0], points):
-                print(three_points, plane[1])
-                faces.append(plane[1])
+            plane, vect_normally, D = count_plane_three_points(three_points)
+            sign_inequality = is_one_side(plane, points)
+            if sign_inequality != 0:
+                faces.append([plane, vect_normally, D, sign_inequality])
+    return faces
 
-def surface_plot(points):
-    x = [point.x for point in points]
-    y = [point.y for point in points]
-    z = [point.z for point in points]
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+def get_presentation_with_xyz(vector, var):
+    if vector == 1: return var
+    if vector == -1: return '-' + var
+    if vector == 0: return ''
+    return str(vector) + var
 
-    ax.voxels(list(x,y,z))
 
-    plt.show()
+def do_change_signs(ax, by, cz):
+    if ((ax != '' and ax[0] == '-') or (ax == '' and by != '' and by[0] == '-')
+            or (ax == '' and by == '' and cz != '' and cz[0] == '-')): return True, -1
+    return False, 1
+
+
+def get_print_plane(data_plane):
+    vect_normally, D, sign_inequality = data_plane[1:]
+
+    ax = get_presentation_with_xyz(vect_normally.x, 'x')
+    by = get_presentation_with_xyz(vect_normally.y, 'y')
+    cz = get_presentation_with_xyz(vect_normally.z, 'z')
+
+    line = []
+    change, int_change = do_change_signs(ax, by, cz)
+
+    signs_math_expression = ['- ', '+ '] if change else ['+ ', '- ']
+
+    add_sign = lambda expr: signs_math_expression[0] + expr if coefs[0] != '-' else signs_math_expression[1] + expr[1:]
+
+    for coefs in (ax, by, cz):
+        if coefs == '': continue
+        if len(line):
+            coefs = add_sign(coefs)
+        else:
+            coefs = coefs[-1]
+        line.append(coefs)
+
+    line.append(">=" if sign_inequality * int_change > 0 else "<=")
+    line.append(str(D * (-1) * int_change))
+    return ' '.join(line)
+
+
+def print_correct_planes(faces):
+    planes = []
+
+    def get_list_all_planes():
+        for data_plane in faces:
+            planes.append(get_print_plane(data_plane))
+
+    def change_if_first_minus():
+        for idx, plane in enumerate(planes):
+            if plane[0] == '-':
+                plane = plane.replace("+", "-").replace("-", "+").replace(">=", "<=")
+                planes[idx] = plane
+
+    get_list_all_planes()
+    change_if_first_minus()
+    planes = list(set(planes))
+    planes.sort()
+    print(f"Number of faces: {len(planes)}")
+    print("\n".join(planes))
 
 
 def main():
-    filepath = r'tets/test_1_tetrahedron.txt'
+    filepath = r'tets/new_test.txt'
     is_file(filepath)
 
     file_format, points = read_coordinates(filepath)
 
     if file_format == 'V':
-        find_convex_hull(points)
+        faces = find_convex_hull(points)
+        print_correct_planes(faces)
+    elif file_format == 'H':
+        print(points)
 
 
 if __name__ == '__main__':
