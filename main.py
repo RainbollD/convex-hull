1f91fd6b91fb23f48a697a8b1802212e64b23d94
Lev <rogulev0805@gmail.com>
2025-04-16 10:40:04 +0300

    add: read from cmd, visualization figure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from itertools import combinations
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
@@ -73,33 +74,50 @@ class PointsABCD:
         return (self.a, self.b, self.c, self.d)
 
 
-def is_file(filepath):
-    if not os.path.exists(filepath):
-        print("Файл не найден")
-        quit(1)
+def is_file(filepaths):
+    """Checking for file"""
+    works_filepath = []
+    for filepath in filepaths:
+        if not os.path.exists(filepath):
+            print(f"Файл {filepath} не найден.\n")
+            continue
+        works_filepath.append(filepath)
+    return works_filepath
 
 
 def read_coordinates(filepath):
+    """
+    Functions:
+    1) Read data from a file
+    2) Write points to PointXYZ if they're coordinates and to PointABCD - plane coefficients
+    :param filepath: file path
+    :return: file format (V, H), class PointXYZ/PointABCD with points
+    """
     try:
         with open(filepath, 'r') as file:
             file_content = file.read()
 
         file_format = file_content[0]
         coordinates = file_content[4:].split('\n')
-        points = []
 
         if file_format == 'V':
             points = [PointsXYZ(*list(map(int, line.strip().split()))) for line in coordinates if line.strip()]
         elif file_format == 'H':
             points = [PointsABCD(*list(map(int, line.strip().split()))) for line in coordinates if line.strip()]
+        else:
+            print(f"Формат вида '{file_format}' не обрабатывается.\n"
+                  f"Проверьте корректность данных в файле!\n"
+                  f"Доступные форматы: V, H")
+            quit(0)
         return file_format, points
 
     except Exception as e:
         print(f"Ошибка при чтении файла: {e}")
-        quit(1)
+        quit(0)
 
 
 def is_one_line(coord):
+    """Check whether the vectors line on the same line"""
     a, b, c = coord
     vect_ab = b - a
     vect_ac = c - a
@@ -107,6 +125,7 @@ def is_one_line(coord):
 
 
 def count_plane_three_points(coord):
+    """Сoefficient calculation for the plane"""
     a, b, c = coord
 
     vect_ab = b - a
@@ -124,6 +143,7 @@ def create_plane_formula(plane_coef):
 
 
 def is_one_side(plane, points):
+    """Check if the points are on the same side"""
     results = [plane(point.x, point.y, point.z) for point in points]
 
     if all(r >= 0 for r in results): return 1
@@ -133,12 +153,22 @@ def is_one_side(plane, points):
 
 
 def is_double_planes(plane_coef, faces):
+    """Check if the same plane in list"""
     for face in faces:
         if plane_coef == face: return True
     return False
 
 
 def find_convex_hull(points):
+    """
+    Finding the convex hull.
+    Functions:
+    1) Construct combinations of 3 points
+    2) Calculate the plane
+    3) Check the side for points
+    :param points: points in the space
+    :return: list of planes coefficients'
+    """
     faces = []
 
     for three_points in combinations(points, 3):
@@ -160,6 +190,7 @@ def find_convex_hull(points):
 
 
 def create_view_plane(coefs):
+    """Creating a beautiful mapping for a plane of type x + y + z <= 0"""
     line = ''
     variables = ['x', 'y', 'z']
 
@@ -178,6 +209,7 @@ def create_view_plane(coefs):
 
 
 def create_list_view_planes(planes):
+    """Create list of beautiful mapping for planes"""
     line = []
     for plane in planes:
         line.append(create_view_plane(plane))
@@ -197,6 +229,7 @@ def transform_float_to_int(top):
 
 
 def count3x3(three_points):
+    """Calculating det for a 3X3 matrix"""
     one, two, three = three_points
     return (one.a * (two.b * three.c - two.c * three.b) -
             one.b * (two.a * three.c - two.c * three.a) +
@@ -204,6 +237,8 @@ def count3x3(three_points):
 
 
 def count_3x3_first_d(three_points, delta_a):
+    """Calculating det for a 3X3 matrix.
+     First column is d"""
     one, two, three = three_points
     return transform_float_to_int((one.d * (two.b * three.c - two.c * three.b) -
                                    one.b * (two.d * three.c - two.c * three.d) +
@@ -211,6 +246,8 @@ def count_3x3_first_d(three_points, delta_a):
 
 
 def count_3x3_second_d(three_points, delta_a):
+    """Calculating det for a 3X3 matrix.
+     Second column is d"""
     one, two, three = three_points
     return transform_float_to_int((one.a * (two.d * three.c - two.c * three.d) -
                                    one.d * (two.a * three.c - two.c * three.a) +
@@ -218,6 +255,8 @@ def count_3x3_second_d(three_points, delta_a):
 
 
 def count_3x3_third_d(three_points, delta_a):
+    """Calculating det for a 3X3 matrix.
+     Third column is d"""
     one, two, three = three_points
     return transform_float_to_int((one.a * (two.b * three.d - two.d * three.b) -
                                    one.b * (two.a * three.d - two.d * three.a) +
@@ -225,6 +264,7 @@ def count_3x3_third_d(three_points, delta_a):
 
 
 def count_delta(three_points):
+    """Calculate the coordinates of the vertex"""
     delta_a = count3x3(three_points)
 
     if delta_a == 0:
@@ -245,6 +285,14 @@ def is_vertex_satisfies_inequalities(vertex, points):
 
 
 def find_vertices(points):
+    """
+    Finding the vertices.
+    Functions:
+    1) Construct combinations of 3 points
+    2) Calculate the vertices
+    :param points: points in the space
+    :return: set of vertices'
+    """
     vertices = set()
 
     for three_points in combinations(points, 3):
@@ -259,6 +307,7 @@ def find_vertices(points):
 
 
 def full_names_vertices(vertices):
+    """Connect the letters to the vertex coordinates"""
     names = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
              "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
@@ -281,6 +330,7 @@ def print_vertices(vertices):
 
 
 def is_vertex_on_face(vertex, face):
+    """Check if the vertex is on the face"""
     return abs(face.a * vertex.x + face.b * vertex.y + face.c * vertex.z - face.d) < 1e-8
 
 
@@ -345,13 +395,23 @@ def print_face_info(faces, vertices):
         print()
 
 
+def print_collides(collides):
+    print("\nCollision detection:")
+    print("The polyhedrons", "intersect" if collides else "do not intersect")
+
+
 def minkowski_difference(poly1, poly2):
+    collides = True
+
+    if poly1 == None or poly2 == None: return
+
     all_faces = poly1 + poly2
 
     for face in all_faces:
         if face.a * 0 + face.b * 0 + face.c * 0 > face.d + 1e-8:
-            return False
-    return True
+            collides = False
+
+    print_collides(collides)
 
 
 def get_face_vertices(faces, vertices):
@@ -366,7 +426,18 @@ def get_face_vertices(faces, vertices):
     return face_vertices
 
 
-def plot_polyhedron(vertices, faces, ax, color='b', alpha=0.2):
+def preparation_plot_polyhedron(vertices, faces, named_vertices):
+    """Create environment for figure"""
+    for vertice, face,  named_vertice in zip(vertices, faces, named_vertices):
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(111, projection='3d')
+        plot_polyhedron(vertice, face, named_vertice,ax)
+        plt.title("3D")
+        plt.show()
+
+
+def plot_polyhedron(vertices, faces, named_vertices, ax, color='b', alpha=0):
+    """Figure visualization"""
     face_verts = get_face_vertices(faces, vertices)
 
     for fv in face_verts:
@@ -378,6 +449,10 @@ def plot_polyhedron(vertices, faces, ax, color='b', alpha=0.2):
     ys = [v.y for v in vertices]
     zs = [v.z for v in vertices]
     ax.scatter(xs, ys, zs, color=color, s=50)
+
+    for vert in named_vertices:
+        label = f"{vert[0]}({vert[1].x}, {vert[1].y}, {vert[1].z})"
+        ax.text(vert[1].x, vert[1].y, vert[1].z, label, color='purple', fontsize=12)
 
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
@@ -398,8 +473,21 @@ def plot_polyhedron(vertices, faces, ax, color='b', alpha=0.2):
 
 
 def process_file(filepath):
-    is_file(filepath)
+    """
+    Function:
+    1) Control actions for different file format
+    2) Finding and printing *convex hull
+                            *vertices
+                            *adjacency matrix
+    :param filepath:
+    :return: faces, vertices <type: list>
+    """
+
+
     file_format, points = read_coordinates(filepath)
+
+    print(f"Actions with file at the path: {filepath}\n"
+          f"\nFile format: {file_format}\n")
 
     if file_format == 'V':
         faces = find_convex_hull(points)
@@ -413,32 +501,56 @@ def process_file(filepath):
     adj_matrix = build_adjacency_matrix(named_vertices, faces)
     print_adjacency_matrix(named_vertices, adj_matrix)
     print_face_info(faces, named_vertices)
+    return faces, vertices, named_vertices
 
-    return faces, vertices
+
+def get_info_console():
+    """Get list of files name's"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", nargs='+', help="Введите файлы")
+    args = parser.parse_args()
+    return args.input
 
 
 def main():
-    filepath = r'tets/test_4_H_tetrahedron.txt'
-    faces1, vertices1 = process_file(filepath)
+    """
+    Main control function
+    1) make action with 1 or 2 files
+    2) calculating minkowski difference for 2 files
+    3) Visualization figures
+    :return:
+    """
+    string_console_data = get_info_console()
 
-    filepath = r'tets/test_3_octahedron.txt'
-    faces2, vertices2 = process_file(filepath)
+    filepaths = is_file(string_console_data)
 
-    collides = minkowski_difference(faces1, faces2)
-    print("\nCollision detection:")
-    print("The polyhedrons", "intersect" if collides else "do not intersect")
+    if len(filepaths) == 2:
+        filepath = filepaths[0]
+        faces1, vertices1, named_vertices1 = process_file(filepath)
 
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection='3d')
-    plot_polyhedron(vertices1, faces1, ax)
-    plt.title("3D Polyhedron")
-    plt.show()
+        filepath = filepaths[1]
+        faces2, vertices2, named_vertices2 = process_file(filepath)
 
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection='3d')
-    plot_polyhedron(vertices2, faces2, ax)
-    plt.title("3D Polyhedron")
-    plt.show()
+        minkowski_difference(faces1, faces2)
+
+        vertices = [vertices1, vertices2]
+        faces = [faces1, faces2]
+        named_vertices = [named_vertices1, named_vertices2]
+
+    elif len(filepaths) == 1:
+        filepath = filepaths[0]
+        faces1, vertices1, named_vertices = process_file(filepath)
+
+        vertices = [vertices1]
+        faces = [faces1]
+        named_vertices = [named_vertices]
+
+    else:
+        print(f"Неправильные данные.\n"
+              f"Количество путей к файлу должно быть: 1 или 2")
+        quit(0)
+
+    preparation_plot_polyhedron(vertices, faces, named_vertices)
 
 
 if __name__ == '__main__':
